314bcbaabad1788f18b80de3f494865fc6635b45
Ali gad <aligad.17611@gmail.com>
2025-01-24 22:37:44 +0200

    update sounds paths

--- lib/features/child/dross/views/widgets/letter_to_word.dart
+++ lib/features/child/dross/views/widgets/letter_to_word.dart
@@ -38,24 +38,3 @@ class LetterToWord extends StatelessWidget {
     );
   }
 }
-
-
-  
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       alignment: Alignment.center,
-//       decoration: BoxDecoration(
-//         color: Colors.white,
-//         borderRadius: BorderRadius.circular(30),
-//       ),
-//       child: Text(
-//         letter,
-//         style: const TextStyle(
-//           fontSize: 24,
-//           fontWeight: FontWeight.bold,
-//         ),
-//       ),
-//     );
-//   }
-// }
