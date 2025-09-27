e4c6ae071985aae12359be6c218ca9e7f8806188
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-09-27 14:32:11 +0300

    Remove unused import from app_usage_stats_view.dart to clean up code

--- lib/features/parent_profile/views/widgets/app_usage_stats_view.dart
+++ lib/features/parent_profile/views/widgets/app_usage_stats_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
-import 'package:yosrixia/core/utils/constants.dart';
 import 'package:yosrixia/core/utils/styles.dart';
 import 'package:yosrixia/core/widgets/app_usage_display.dart';
 
