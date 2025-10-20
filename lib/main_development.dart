aa54a77116328e4a2938aa935ad5ba03df74b7e4
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-10-20 22:23:56 +0300

    feat: add flavors without change app id till create new app in firebase

---
+++ lib/main_development.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:yosrixia/core/di/dependency_injection.dart';
+import 'package:yosrixia/core/services/app_usage_tracker.dart';
+import 'package:yosrixia/features/subscripton_and_payments/manger/services/payment_services.dart';
+import 'package:yosrixia/firebase_options.dart';
+import 'package:yosrixia/supabase_config.dart';
+import 'package:yosrixia/yosrixia_app.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  //* initialize paymob
+  await PaymentServices.initPaymob();
+  //* initialize hive
+  await Hive.initFlutter();
+  //* initialize Dependencies
+  await setupDependencyInjection();
+
+  // Initialize app usage tracker
+  await AppUsageTracker().init();
+
+  await SupabaseConfig.init();
+  runApp(const YosrixiaApp());
+}
