411a4e4c7140d37f2c77501b31d65be9c2d6b32b
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-10-22 11:54:07 +0300

    feat: enhance app initialization and dependency injection
    - Added Firebase and Supabase initialization in main development and production files.
    - Updated dependency injection setup to include FirebaseFirestore and SupabaseClient.
    - Introduced Picker class for image selection and AppLogger class for logging information.
    - Refactored import paths for profile views to align with new structure.

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,8 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:yosrixia/features/child/profile/data/dataSources/profile_service.dart';
+import 'package:yosrixia/features/child/profile/data/dataSources/storage_service.dart';
+import 'package:yosrixia/features/child/profile/data/repo/profile_repo.dart';
+import 'package:yosrixia/features/child/profile/presentation/cubits/child_info_cubit/child_info_cubit.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> setupDependencyInjection() async {
+
+  //====== Core ======
   getIt.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
+  getIt.registerSingleton<FirebaseFirestore>(FirebaseFirestore.instance);
+  getIt.registerSingleton<SupabaseClient>(Supabase.instance.client);
+
+  //====== Features ======
+
+  // Profile
+
+  // profile service
+  getIt.registerLazySingleton<ProfileService>(() => ProfileService(auth: getIt<FirebaseAuth>(), firestore: getIt<FirebaseFirestore>()));
+  getIt.registerLazySingleton<StorageService>(() => StorageService(supabase: getIt<SupabaseClient>()));
+  // profile repo
+  getIt.registerLazySingleton<ProfileRepo>(() => ProfileRepo(profileService: getIt<ProfileService>(), storageService: getIt<StorageService>()));
+
+  // profile cubits
+  getIt.registerLazySingleton<ChildInfoCubit>(() => ChildInfoCubit(profileRepo: getIt<ProfileRepo>()));
 }
