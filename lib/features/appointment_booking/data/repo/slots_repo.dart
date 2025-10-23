36f4ea9e1957214e585455c92702e356907fafe5
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-10-23 09:56:10 +0300

    feat: implement SlotsRepo for appointment booking management
    - Created SlotsRepo class to manage appointment slots, including methods for adding, booking, deleting, and streaming available slots.
    - Integrated with SlotsService to handle backend operations and error management using Either type for success and failure states.

---
+++ lib/features/appointment_booking/data/repo/slots_repo.dart
@@ -0,0 +1,57 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:yosrixia/core/error/failure.dart';
+import 'package:yosrixia/core/success/success.dart';
+import 'package:yosrixia/features/appointment_booking/data/models/slot_model.dart';
+import 'package:yosrixia/features/appointment_booking/data/services/slots_service.dart';
+
+class SlotsRepo {
+  final SlotsService _slotsService;
+  SlotsRepo({required SlotsService slotsService})
+      : _slotsService = slotsService;
+
+  //* add slot
+  Future<Either<Failure, Success>> addSlot(SlotModel slot) async {
+    try {
+      await _slotsService.addSlot(slot);
+      return const Right(
+          Success(icon: Icons.check, message: 'تم إضافة الموعد بنجاح'));
+    } catch (e) {
+      return Left(Failure(icon: Icons.error, message: e.toString()));
+    }
+  }
+
+  //* get available slots
+  Stream<Either<Failure, List<SlotModel>>> getAvailableSlotsStream(
+      String doctorId) async* {
+    try {
+      final slots = _slotsService.getAvailableSlotsStream(doctorId);
+      yield* slots.map((slots) => Right(slots));
+    } catch (e) {
+      yield Left(Failure(icon: Icons.error, message: e.toString()));
+    }
+  }
+
+  //* book slot
+  Future<Either<Failure, Success>> bookSlot(String slotId, String childId,
+      String childName, String childPhone) async {
+    try {
+      await _slotsService.bookSlot(slotId, childId, childName, childPhone);
+      return const Right(
+          Success(icon: Icons.check, message: 'تم حجز الموعد بنجاح'));
+    } catch (e) {
+      return Left(Failure(icon: Icons.error, message: e.toString()));
+    }
+  }
+
+  //* delete slot
+  Future<Either<Failure, Success>> deleteSlot(String slotId) async {
+    try {
+      await _slotsService.deleteSlot(slotId);
+      return const Right(
+          Success(icon: Icons.check, message: 'تم حذف الموعد بنجاح'));
+    } catch (e) {
+      return Left(Failure(icon: Icons.error, message: e.toString()));
+    }
+  }
+}
