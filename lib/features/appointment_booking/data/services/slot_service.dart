930bf76114f3db61271f264f4b200ce520b692b0
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-10-23 09:05:42 +0300

    feat: implement SlotService for appointment booking management
    - Added SlotService class to handle appointment slot operations including adding, retrieving, booking, and deleting slots.
    - Integrated Firestore for data storage and retrieval, ensuring slots are managed effectively with availability checks.
    - Implemented methods for adding new slots, streaming available slots, booking a slot, and deleting a slot.

---
+++ lib/features/appointment_booking/data/services/slot_service.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:yosrixia/features/appointment_booking/data/models/slot_model.dart';
+
+class SlotService {
+  final FirebaseFirestore _firestore;
+  final String collectionName = 'appointment_slots';
+  SlotService({required FirebaseFirestore firestore}) : _firestore = firestore;
+
+  //* add slot
+  Future<void> addSlot(SlotModel slot) async {
+    //* check if slot already exists
+    final existingSlot = await _firestore
+        .collection(collectionName)
+        .where('doctorId', isEqualTo: slot.doctorId)
+        .where('dateTime', isEqualTo: slot.dateTime)
+        .get();
+    if (existingSlot.docs.isNotEmpty) {
+      throw Exception('هناك موعد موجود بالفعل في هذا الوقت');
+    }
+    await _firestore.collection(collectionName).add(slot.toJson());
+  }
+
+  //* get slots from firestore
+  Stream<List<SlotModel>> getAvailableSlotsStream(String doctorId) async* {
+    yield* _firestore
+        .collection(collectionName)
+        .where('doctorId', isEqualTo: doctorId)
+        .where('isAvailable', isEqualTo: true)
+        .orderBy('dateTime', descending: false)
+        .snapshots()
+        .map((snapshot) => snapshot.docs
+            .map((doc) => SlotModel.fromJson(doc.data()))
+            .toList());
+  }
+
+  //* book slot
+  Future<void> bookSlot(String slotId, String childId, String childName,
+      String childPhone) async {
+    await _firestore.collection(collectionName).doc(slotId).update({
+      'bookedBy': childId,
+      'bookedByName': childName,
+      'bookedByPhone': childPhone,
+      'isAvailable': false,
+    });
+  }
+
+  //* delete slot
+  Future<void> deleteSlot(String slotId) async {
+    await _firestore.collection(collectionName).doc(slotId).delete();
+  }
+}
